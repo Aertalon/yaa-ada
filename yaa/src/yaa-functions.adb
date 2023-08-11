bb495d8cd7c19b5f73c23eddea4a28fa7829f7f9
ELovisari <elovisari@gmail.com>
2023-08-11 21:55:29 +0200

    Add (almost) all functions in Generic_Elementary_Functions

--- yaa/src/yaa-functions.adb
+++ yaa/src/yaa-functions.adb
@@ -1,5 +1,9 @@
 package body YAA.Functions is
 
+   One : constant Real := Real (1.0);
+   Two : constant Real := Real (2.0);
+   Four : constant Real := Real (4.0);
+
    --  For any sufficiently continuous function (?)
    --  f(a0 + a1e1 + a2e2 + a12e12)
    --    = f(a0) + (a1e1 + a2e2 + a12e12)f'(a0)
@@ -10,13 +14,228 @@ package body YAA.Functions is
    --    = f(a0) + (a1e1 + a2e2 + a12e12)f'(a0) + 0.5*(2a1a2)^2f''(a0)
    --    = f(a0) + a1f'(a0)e1 + a2f'(a0)e2 + (a12f'(a0) + a1a2f''(a0))e12
 
-   function Cos (X : Dual_Types.Dual) return Dual_Types.Dual is
+   function DerivativesToDual (X : Dual; F, FPrime, FSecond : Real)
+   return Dual is
    begin
       return (
-         Re => CosR (X.Re),
-         Im1 => -X.Im1 * SinR (X.Re),
-         Im2 => -X.Im2 * SinR (X.Re),
-         Im12 => -X.Im12 * SinR (X.Re) - X.Im1 * X.Im2 * CosR (X.Re));
+         Re => F,
+         Im1 => X.Im1 * FPrime,
+         Im2 => X.Im2 * FPrime,
+         Im12 => X.Im12 * FPrime + X.Im1 * X.Im2 * FSecond);
+   end DerivativesToDual;
+
+   --  f(x) = x^q
+   --  f'(x) = qx^(q-1)
+   --  f''(x) = q * (q-1) * x^(q-2)
+   function Power (X : Dual; Q : Real) return Dual is
+      F : constant Real := PowerR (X.Re, Q);
+      FPrime : constant Real := Q * PowerR (X.Re, Q - One);
+      FSecond : constant Real := Q * (Q - One) * PowerR (X.Re, Q - Two);
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Power;
+
+   --  f(x) = x^0.5
+   --  f'(x) = 0.5x^-0.5 = 1 / (2 * f(x))
+   --  f''(x) = -0.25x^-1.5 = - 1/( 4 * f'(x)^3)
+   function Sqrt (X : Dual) return Dual is
+      F : constant Real := SqrtR (X.Re);
+      FPrime : constant Real := One / (Two * F);
+      FSecond : constant Real := -One / (Four * F * F * F);
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Sqrt;
+
+   --  f(x) = log(x)
+   --  f'(x) = x^-1
+   --  f''(x) = -x^-2 = - f'(x)^2
+   function Log (X : Dual) return Dual is
+      F : constant Real := LogR (X.Re);
+      FPrime : constant Real := One / (X.Re);
+      FSecond : constant Real := -FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Log;
+
+   --  f(x) = exp(x)
+   --  f'(x) = exp(x)
+   --  f''(x) = exp(x)
+   function Exp (X : Dual) return Dual is
+      F : constant Real := ExpR (X.Re);
+   begin
+      return DerivativesToDual (X, F, F, F);
+   end Exp;
+
+   --  f(x) = cos(x)
+   --  f'(x) = -sin(x)
+   --  f''(x) = -cos(x)
+   function Cos (X : Dual) return Dual is
+      F : constant Real := CosR (X.Re);
+      FPrime : constant Real := -SinR (X.Re);
+   begin
+      return DerivativesToDual (X, F, FPrime, -F);
    end Cos;
+
+   --  f(x) = sin(x)
+   --  f'(x) = cos(x)
+   --  f''(x) = -sinx(x)
+   function Sin (X : Dual) return Dual is
+      F : constant Real := SinR (X.Re);
+      FPrime : constant Real := CosR (X.Re);
+   begin
+      return DerivativesToDual (X, F, FPrime, -F);
+   end Sin;
+
+   --  f(x) = tan(x)
+   --  f'(x) = 1 + tan^2(x)
+   --  f''(x) = 2 tan(x) (tan(x))'
+   function Tan (X : Dual) return Dual is
+      F : constant Real := TanR (X.Re);
+      FPrime : constant Real := One + F * F;
+      FSecond : constant Real := Two * F * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Tan;
+
+   --  f(x) = cot(x)
+   --  f'(x) = -1 - cot^2(x)
+   --  f''(x) = - 2 cot(x) (cot(x))'
+   function Cot (X : Dual) return Dual is
+      F : constant Real := CotR (X.Re);
+      FPrime : constant Real := -One - F * F;
+      FSecond : constant Real := -Two * F * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Cot;
+
+   --  f(x) = arccos(x)
+   --  f'(x) = -(1 - x^2)^(-0.5)
+   --  f''(x) = -0.5 * 2x * (1 - x^2)^(-1.5) = -x * f'(x)^3
+   function Arccos (X : Dual) return Dual is
+      F : constant Real := ArccosR (X.Re);
+      FPrime : constant Real := -One / SqrtR (One - X.Re * X.Re);
+      FSecond : constant Real := -X.Re * FPrime * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arccos;
+
+   --  f(x) = arcsin(x)
+   --  f'(x) = (1 - x^2)^(-0.5)
+   --  f''(x) = 0.5 * 2x * (1 - x^2)^(-1.5) = x * f'(x)^3
+   function Arcsin (X : Dual) return Dual is
+      F : constant Real := ArcsinR (X.Re);
+      FPrime : constant Real := One / SqrtR (One - X.Re * X.Re);
+      FSecond : constant Real := X.Re * FPrime * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arcsin;
+
+   --  f(x) = arctan(x)
+   --  f'(x) = (1 + x^2)^(-1)
+   --  f''(x) = - 2x * (1 - x^2)^(-2) = -2x * f'(x)^2
+   function Arctan (X : Dual) return Dual is
+      F : constant Real := ArctanR (X.Re);
+      FPrime : constant Real := One / (One + X.Re * X.Re);
+      FSecond : constant Real := -Two * X.Re * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arctan;
+
+   --  f(x) = arccot(x)
+   --  f'(x) = -(1 + x^2)^(-1)
+   --  f''(x) = 2x * (1 - x^2)^(-2) = 2x * f'(x)^2
+   function Arccot (X : Dual) return Dual is
+      F : constant Real := ArccotR (X.Re);
+      FPrime : constant Real := -One / (One + X.Re * X.Re);
+      FSecond : constant Real := Two * X.Re * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arccot;
+
+   --  f(x) = cosh(x)
+   --  f'(x) = sinh(x)
+   --  f''(x) = cosh(x)
+   function Cosh (X : Dual) return Dual is
+      F : constant Real := CoshR (X.Re);
+      FPrime : constant Real := SinhR (X.Re);
+   begin
+      return DerivativesToDual (X, F, FPrime, F);
+   end Cosh;
+
+   --  f(x) = sinh(x)
+   --  f'(x) = cosh(x)
+   --  f''(x) = sinh(x)
+   function Sinh (X : Dual) return Dual is
+      F : constant Real := SinhR (X.Re);
+      FPrime : constant Real := CoshR (X.Re);
+   begin
+      return DerivativesToDual (X, F, FPrime, F);
+   end Sinh;
+
+   --  f(x) = tanh(x)
+   --  f'(x) = 1 - tanh(x)^2
+   --  f''(x) = -2tanh(x)(tanh(x))'
+   function Tanh (X : Dual) return Dual is
+      F : constant Real := TanhR (X.Re);
+      FPrime : constant Real := One - F * F;
+      FSecond : constant Real := -Two * F * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Tanh;
+
+   --  f(x) = coth(x)
+   --  f'(x) = 1 - coth(x)^2
+   --  f''(x) = -coth(x)(coth(x))'
+   function Coth (X : Dual) return Dual is
+      F : constant Real := CothR (X.Re);
+      FPrime : constant Real := One - F * F;
+      FSecond : constant Real := -Two * F * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Coth;
+
+   --  f(x) = arccosh(x)
+   --  f'(x) = (x^2 - 1)^-0.5
+   --  f''(x) = -0.5 *2x * (x^2 - 1)^-1.5 = -x * f'(x)^3
+   function Arccosh (X : Dual) return Dual is
+      F : constant Real := ArccoshR (X.Re);
+      FPrime : constant Real := One / SqrtR (X.Re * X.Re - One);
+      FSecond : constant Real := -X.Re * FPrime * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arccosh;
+
+   --  f(x) = arcsinh(x)
+   --  f'(x) = (x^2 + 1)^-0.5
+   --  f''(x) = -0.5 *2x * (x^2 + 1)^-1.5 = -x * f'(x)^3
+   function Arcsinh (X : Dual) return Dual is
+      F : constant Real := ArcsinhR (X.Re);
+      FPrime : constant Real := One / SqrtR (X.Re * X.Re + One);
+      FSecond : constant Real := -X.Re * FPrime * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arcsinh;
+
+   --  f(x) = arctanh(x)
+   --  f'(x) = (1 - x^2)^-1
+   --  f''(x) = 2x * (1 - x^2)^-2 = 2x * f'(x)^2
+   function Arctanh (X : Dual) return Dual is
+      F : constant Real := ArctanhR (X.Re);
+      FPrime : constant Real := One / (One - X.Re * X.Re);
+      FSecond : constant Real := Two * X.Re * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arctanh;
+
+   --  f(x) = arccoth(x)
+   --  f'(x) = -(1 - x^2)^-1
+   --  f''(x) = -2x * (1 - x^2)^-2 = -2x * f'(x)^2
+   function Arccoth (X : Dual) return Dual is
+      F : constant Real := ArctanhR (X.Re);
+      FPrime : constant Real := -One / (One - X.Re * X.Re);
+      FSecond : constant Real := -Two * X.Re * FPrime * FPrime;
+   begin
+      return DerivativesToDual (X, F, FPrime, FSecond);
+   end Arccoth;
 
 end YAA.Functions;
--- yaa/src/yaa-functions.ads
+++ yaa/src/yaa-functions.ads
@@ -1,4 +1,5 @@
 with Ada.Numerics.Generic_Elementary_Functions;
+
 generic
 package YAA.Functions is
 
@@ -7,9 +8,56 @@ package YAA.Functions is
    package Functions_On_Real is new
       Ada.Numerics.Generic_Elementary_Functions (Real);
 
+   function PowerR (X : Real; Q: Real) return Real renames Functions_On_Real."**";
+
+   function SqrtR (X : Real) return Real renames Functions_On_Real.Sqrt;
+   function LogR (X : Real) return Real renames Functions_On_Real.Log;
+   function ExpR (X : Real) return Real renames Functions_On_Real.Exp;
+
    function CosR (X : Real) return Real renames Functions_On_Real.Cos;
    function SinR (X : Real) return Real renames Functions_On_Real.Sin;
+   function TanR (X : Real) return Real renames Functions_On_Real.Tan;
+   function CotR (X : Real) return Real renames Functions_On_Real.Cot;
 
-   function Cos (X : Dual_Types.Dual) return Dual_Types.Dual;
+   function ArccosR (X : Real) return Real renames Functions_On_Real.Arccos;
+   function ArcsinR (X : Real) return Real renames Functions_On_Real.Arcsin;
+   function ArctanR (X : Real; Y : Real := Real(1.0)) return Real renames Functions_On_Real.Arctan;
+   function ArccotR (X : Real; Y : Real := Real(1.0)) return Real renames Functions_On_Real.Arccot;
+
+   function CoshR (X : Real) return Real renames Functions_On_Real.Cosh;
+   function SinhR (X : Real) return Real renames Functions_On_Real.Sinh;
+   function TanhR (X : Real) return Real renames Functions_On_Real.Tanh;
+   function CothR (X : Real) return Real renames Functions_On_Real.Coth;
+
+   function ArccoshR (X : Real) return Real renames Functions_On_Real.Arccosh;
+   function ArcsinhR (X : Real) return Real renames Functions_On_Real.Arcsinh;
+   function ArctanhR (X : Real) return Real renames Functions_On_Real.Arctanh;
+   function ArccothR (X : Real) return Real renames Functions_On_Real.Arccoth;
+
+   function Power (X : Dual; Q : Real) return Dual;
+
+   function Sqrt (X : Dual) return Dual;
+   function Log (X : Dual) return Dual;
+   function Exp (X : Dual) return Dual;
+
+   function Cos (X : Dual) return Dual;
+   function Sin (X : Dual) return Dual;
+   function Tan (X : Dual) return Dual;
+   function Cot (X : Dual) return Dual;
+
+   function Arccos (X : Dual) return Dual;
+   function Arcsin (X : Dual) return Dual;
+   function Arctan (X : Dual) return Dual;
+   function Arccot (X : Dual) return Dual;
+
+   function Cosh (X : Dual) return Dual;
+   function Sinh (X : Dual) return Dual;
+   function Tanh (X : Dual) return Dual;
+   function Coth (X : Dual) return Dual;
+
+   function Arccosh (X : Dual) return Dual;
+   function Arcsinh (X : Dual) return Dual;
+   function Arctanh (X : Dual) return Dual;
+   function Arccoth (X : Dual) return Dual;
 
 end YAA.Functions;
